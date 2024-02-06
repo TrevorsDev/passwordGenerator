d7a8b5959659a9f23577f273af1537a061e8ca72
Trevor Johnson <trevor9j@protonmail.com>
2024-02-05 18:25:50 -0700

    created a parameter to limit the password length to between 8-128 characters, added an alert if length not met.

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+//when I enter the password I want it to be 8-128 characters long
+//if the psswrd is btwn 8-128 generate psswrd
+//if less than 8 or greater than 128 reject input/prompt too short or too long
+
 /* We begin with a constant variable called gereatePassword which is equal to an arrow function that will
 hold all of the programming needed to generate a password when a user clicks the button "generate password
 on the web page*/
 
 const generatePassword = () => {
 
-/*Inside the function, I then label and set our main variables equal to the characters that will be used in 
-any generated password. Separate variables are defined based on thier relationship w/each other. Ex: uppercase letters
-numbers, etc*/
+  /*Inside the function, I then label and set our main variables equal to the characters that will be used in 
+  any generated password. Separate variables are defined based on thier relationship w/each other. Ex: uppercase letters
+  numbers, etc*/
   var specialCharacters = '!@#$%^&*'.split('');
   var numbers = '123456789'.split('');
   var lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
   var upperCase = 'ABCDEFGHIJKLMOPQRSTUVWXYZ'.split('');
+
+  const psswrdLength = prompt("How long do you want your password to be?");
+
+  if (psswrdLength >= 8 && psswrdLength <= 128) {
+    console.log("confirmChoices");
+  } else {
+    alert("Pick a better length");
+    return;
+  }
+
 
   let confirmChoices = []
 
@@ -22,7 +36,8 @@ numbers, etc*/
   const needsLower = confirm("Do you want lower case letters?")
   const needsSpecial = confirm("Do you want special characters?");
   const needsNumbers = confirm("Do you want numbers?");
-  const psswrdLength = prompt("How long do you want your password to be?");
+
+
 
   /*Here, 'if' statements are created which individually refer to the users choice of characters for their password. If the user
   confirms that they indeed want capital letters, then needsCapital is equal to 'true' and 'confirmChoices' is ran, so that the
@@ -41,15 +56,20 @@ numbers, etc*/
     confirmChoices = confirmChoices.concat(numbers);
   }
   console.log(confirmChoices)
-/*A new variable called password is set equal to an empty string.  */
+
+  /*A new variable called password is set equal to an empty string.  */
   let password = "";
-/*A loop is created with a local variable assgined a name of randomCharacter and set equal to the variable confrimChoices (which holds all
-  of the users confirmed characters for their password). This value is multiplying the length of the users character choices by a random
-  number between 0 and .999 with 'Math.random'. That number is then round down with Math.floor. The password is then */ 
+  /*A loop is created with a local variable assgined a name of randomCharacter and set equal to the variable confrimChoices (which holds all
+    of the users confirmed characters for their password). This value is multiplying the length of the users character choices by a random
+    number between 0 and .999 with 'Math.random'. That number is then round down with Math.floor. The password is then */
   for (i = 0; i < psswrdLength; i++) {
     let randomCharacter = confirmChoices[Math.floor(Math.random() * confirmChoices.length)];
     password += randomCharacter;
+
+
   }
+
+
   return password;
 }
 
@@ -58,7 +78,7 @@ Written in words to help me understand better, here 'var generateBtn is created.
 connected by a wire. The wire in this case is 'document.querySelector'. This wire connects the electric guitar to an amplifire. In this case
 the amplifire is the HTML ID 'generate'. The ID 'generate' is attached to a button. So, the HTML button is tangible and something the user
 can interact with, while the JS variable 'generateBtn' is connecting JS to this button via 'document.querySelector'. This allows us as
-programmers to create specific actions in the background after a user presses the HTML button to generate a password.*/ 
+programmers to create specific actions in the background after a user presses the HTML button to generate a password.*/
 var generateBtn = document.querySelector("#generate");
 
 /*Step 3: Once the user has clicked the button, this function runs. Inside, a local variable called 'password' is set equal to the global function
@@ -71,6 +91,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+
   //initial comment for creating new feature branch
 }
 
